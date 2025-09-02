709994148d8073a6341e8883999c406cb5b9ae3b
Mykhailo <kurochkinmy@gmail.com>
2025-09-02 15:42:07 +0300

    fix: fixed generateRandomNumber function logic

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -9,20 +9,20 @@
 function generateRandomNumber() {
   const digits = Array.from({ length: 10 }, (_, i) => i);
 
+  const firstDigitIndex = Math.floor(Math.random() * 9) + 1;
+  const firstDigit = digits[firstDigitIndex];
+
+  digits.splice(firstDigitIndex, 1);
+
   for (let i = digits.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
 
     [digits[i], digits[j]] = [digits[j], digits[i]];
   }
 
-  const number =
-    digits[0] === 0
-      ? digits[1].toString() +
-        digits[0].toString() +
-        digits.slice(2, 4).join('')
-      : digits.slice(0, 4).join('');
+  const numberStr = [firstDigit, ...digits.slice(0, 3)].join('');
 
-  return +number;
+  return +numberStr;
 }
 
 module.exports = {
