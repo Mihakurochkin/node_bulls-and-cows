2588844ec136c649be156e2c649c2a0e8958be01
Mykhailo <kurochkinmy@gmail.com>
2025-09-02 15:21:14 +0300

    feat: implement node bulls ans cows game

--- src/modules/generateRandomNumber.js
+++ src/modules/generateRandomNumber.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { checkIsValidUserInput } = require('./checkIsValidUserInput');
+
 /**
  * Generate a random 4-digit number that does not start with 0
  * and does not contain any duplicate digits.
@@ -7,7 +9,13 @@
  * @return {number} A random 4-digit number
  */
 function generateRandomNumber() {
-  /* Write your code here */
+  let number = Math.floor(Math.random() * 9000) + 1000;
+
+  while (checkIsValidUserInput(String(number)) === false) {
+    number = Math.floor(Math.random() * 9000) + 1000;
+  }
+
+  return number;
 }
 
 module.exports = {
